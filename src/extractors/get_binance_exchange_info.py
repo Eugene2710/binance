a451d8c5b8241bc325dc2677c308d33199ae3307
eugeneleejunping <eugeneleejunping@gmail.com>
2025-06-22 15:48:55 +0800

    integrated pydantic data class into get exhcange info extractor

--- src/extractors/get_binance_exchange_info.py
+++ src/extractors/get_binance_exchange_info.py
@@ -4,6 +4,8 @@ import aiohttp
 from pprint import pprint
 from tenacity import retry, wait_fixed, stop_after_attempt
 
+from src.models.binance_exchange_info import ExchangeInfo
+
 
 class BinanceExchangeInfoExtractor:
     @staticmethod
@@ -12,7 +14,7 @@ class BinanceExchangeInfoExtractor:
         stop=stop_after_attempt(5),
         reraise=True
     )
-    async def extract(symbol: str) -> dict[str, Any]:
+    async def extract(symbol: str) -> ExchangeInfo:
         """
         Get Exchange Ticker Info so that these ticker symbols can be used for further querying against the API
         https://developers.binance.com/docs/binance-spot-api-docs/rest-api/general-endpoints
@@ -30,8 +32,8 @@ class BinanceExchangeInfoExtractor:
                     possible symbol status: PRE_TRADING, TRADING, POST_TRADING, END_OF_DAY, HALT, AUCTION_MATCH, BREAK
                     https://developers.binance.com/docs/binance-spot-api-docs/enums#account-and-symbol-permissions
                     """
-                    # exchange_info: ExchangeInfo = ExchangeInfo.model_validate(data)
-                    return data
+                    exchange_info: ExchangeInfo = ExchangeInfo.model_validate(data)
+                    return exchange_info
                 else:
                     raise Exception(f"Received non-status code 200: {response.status}")
 
@@ -39,6 +41,6 @@ class BinanceExchangeInfoExtractor:
 if __name__ == "__main__":
     binance_exchange_info_extractor: BinanceExchangeInfoExtractor = BinanceExchangeInfoExtractor()
     event_loop: AbstractEventLoop = new_event_loop()
-    res: dict[str, Any] = event_loop.run_until_complete(binance_exchange_info_extractor.extract("ETHUSDT"))
+    res: ExchangeInfo = event_loop.run_until_complete(binance_exchange_info_extractor.extract("ETHUSDT"))
     pprint(res)
 
